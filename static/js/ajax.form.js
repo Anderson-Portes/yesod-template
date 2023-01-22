fd5f37942b0af53eb60a96048a7bab48eb305520
Anderson Portes <portsanderson@gmail.com>
2023-01-22 12:11:43 -0300

    Add textarea elements to formToJson function

--- static/js/ajax.form.js
+++ static/js/ajax.form.js
@@ -1,41 +1,54 @@
-const form = document.getElementById('page-form')
+const form = document.getElementById("page-form");
 
-const renderErrors = errors => {
-  const html = errors.reduce(e => `<span class="d-block">${e}</span>`)
-  form.insertAdjacentHTML('beforebegin', `<div class="alert alert-danger message">${html}</div>`)
+const renderErrors = (errors) => {
+  const html = errors.reduce((e) => `<span class="d-block">${e}</span>`);
+  form.insertAdjacentHTML(
+    "beforebegin",
+    `<div class="alert alert-danger message">${html}</div>`
+  );
 };
 
 const formToJson = () => {
   const data = {};
-  form.querySelectorAll('input').forEach(i => data[i.name] = i.value);
+  form
+    .querySelectorAll("input, textarea")
+    .forEach((i) => (data[i.name] = i.value || i.textContent));
   return JSON.stringify(data);
-}
+};
 
-const clearMessages = () => document.querySelectorAll('.message').forEach(e => e.remove());
+const clearMessages = () =>
+  document.querySelectorAll(".message").forEach((e) => e.remove());
 
 const renderSuccess = (message) => {
-  const redirect = form.getAttribute('redirect-to');
+  const redirect = form.getAttribute("redirect-to");
   redirect && (location.href = redirect);
-  form.getAttribute('reset') == "true" && form.reset();
-  form.insertAdjacentHTML('beforebegin', `<div class="alert alert-success message">${message}</div>`)
-}
+  form.getAttribute("reset") == "true" && form.reset();
+  form.insertAdjacentHTML(
+    "beforebegin",
+    `<div class="alert alert-success message">${message}</div>`
+  );
+};
 
-const handleSubmit = async e => {
+const handleSubmit = async (e) => {
   e.preventDefault();
   clearMessages();
   try {
-    const data = await fetch(form.action, { method: form.method || 'GET', body: formToJson(), headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    } });
+    const data = await fetch(form.action, {
+      method: form.method || "GET",
+      body: formToJson(),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
     const json = await data.json();
     const { errors, success } = json;
     errors && renderErrors(errors);
     success && renderSuccess(success);
   } catch (err) {
-    alert('Some unknown error occurred!')
+    alert("Some unknown error occurred!");
     console.log(err);
   }
-}
+};
 
-form?.addEventListener('submit', handleSubmit);
+form?.addEventListener("submit", handleSubmit);
